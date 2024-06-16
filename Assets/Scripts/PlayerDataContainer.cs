81b1aab6d5816bb99896b3f4a24ee4b892153279
solhwi <psh50zmfhtm@naver.com>
2024-06-17 02:05:30 +0900

    눈을 갈아끼우는 데에는 성공함

--- Assets/Scripts/PlayerDataContainer.cs
+++ Assets/Scripts/PlayerDataContainer.cs
@@ -11,6 +11,9 @@ public class PlayerDataContainer : ScriptableObject
 	public int currentTileOrderIndex = 0;
 	public float moveTimeByOneTile = 1.0f;
 
+	[Header("[(0) 바디 / (1) 바디 피부 / (2) 왼쪽 눈 / (3) 오른쪽 눈 / (4) 앞 머리]")]
+	[Header("[(5) 뒷 머리, (6) 얼굴, (7) 왼쪽 눈 커버 / (8) 오른쪽 눈 커버]")]
+	[Space]
 	public CharacterType[] characterMeshTypes = new CharacterType[(int)CharacterMeshType.Max];
 
 	public void SaveCurrentOrderIndex(int currentTileOrderIndex)
