98b23409da7f7993390ee12d0335ad9f4719df75
psh50zmfhtm <psh50zmfhtm@naver.com>
2024-09-17 22:52:24 +0900

    ㅇㅅㅇ

--- Assets/Scripts/Firebase/AuthManager.cs
+++ Assets/Scripts/Firebase/AuthManager.cs
@@ -13,7 +13,7 @@ public class AuthManager : Singleton<AuthManager>
 
 	private void Start()
 	{
-#if UNITY_WEBGL
+#if !UNITY_EDITOR && UNITY_WEBGL
 		FirebaseAuth.OnAuthStateChanged(gameObject.name, "OnAuthStateChanged", "OnAuthStateChangeFailed");
 #endif
 	}
@@ -35,21 +35,23 @@ public class AuthManager : Singleton<AuthManager>
 		// js에서 data로 메일을 받아오고, 미리 매핑된 데이터와 대조하여 그룹, 이름을 가져온다.
 		onLoginSuccess?.Invoke(data);
 		onLoginSuccess = null;
+		onLoginFailed = null;
 	}
 
 	private void OnLoginFailed(string error)
 	{
 		onLoginFailed?.Invoke(error);
 		onLoginFailed = null;
+		onLoginSuccess = null;
 	}
 
 	private void OnAuthStateChanged(string data)
 	{
-
+		Debug.Log($"{data}");
 	}
 
 	private void OnAuthStateChangeFailed(string error)
 	{
-
+		Debug.LogError($"{error}");
 	}
 }
