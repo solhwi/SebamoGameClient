0bab3a94284ec32c3aa63d7b0a480d2d343a8276
Jihyun0511 <yjhjulia1@gmail.com>
2025-04-13 19:09:21 +0900

    링크드리스트 변경 대응

--- Assets/Scripts/UI/BuffItemComponent.cs
+++ Assets/Scripts/UI/BuffItemComponent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BuffItemComponent : MonoBehaviour
@@ -17,7 +18,7 @@ public class BuffItemComponent : MonoBehaviour
 			}
 			else
 			{
-				string buffItemCode = inventory.appliedBuffItems[i];
+				string buffItemCode = inventory.appliedBuffItems.ElementAt(i);
 				buffItemIcons[i].SetItemData(buffItemCode);
 
 				buffItemIcons[i].gameObject.SetActive(true);
