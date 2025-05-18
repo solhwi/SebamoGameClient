eeba09d23d53a10e2ee8c604f1dfb8f7b0ed6542
solhwi <psh50zmfhtm@naver.com>
2025-05-19 01:41:15 +0900

    WebGL 라이브러리 임시 제거 (안드로이드 빌드 위함)

--- Assets/Scripts/Firebase/FirebaseBridge/FirebaseAuth.cs
+++ Assets/Scripts/Firebase/FirebaseBridge/FirebaseAuth.cs
@@ -2,16 +2,16 @@ using System.Runtime.InteropServices;
 
 namespace FirebaseWebGL.Scripts.FirebaseBridge
 {
-    public static class FirebaseAuth
-    {
-        /// <summary>
-        /// Signs in a user with Google
-        /// </summary>
-        /// <param name="objectName"> Name of the gameobject to call the callback/fallback of </param>
-        /// <param name="callback"> Name of the method to call when the operation was successful. Method must have signature: void Method(string output) </param>
-        /// <param name="fallback"> Name of the method to call when the operation was unsuccessful. Method must have signature: void Method(string output). Will return a serialized FirebaseError object </param>
-        [DllImport("__Internal")]
-        public static extern void SignInWithGoogle(string objectName, string callback,
-            string fallback);
-    }
+    //public static class FirebaseAuth
+    //{
+    //    /// <summary>
+    //    /// Signs in a user with Google
+    //    /// </summary>
+    //    /// <param name="objectName"> Name of the gameobject to call the callback/fallback of </param>
+    //    /// <param name="callback"> Name of the method to call when the operation was successful. Method must have signature: void Method(string output) </param>
+    //    /// <param name="fallback"> Name of the method to call when the operation was unsuccessful. Method must have signature: void Method(string output). Will return a serialized FirebaseError object </param>
+    //    [DllImport("__Internal")]
+    //    public static extern void SignInWithGoogle(string objectName, string callback,
+    //        string fallback);
+    //}
 }
